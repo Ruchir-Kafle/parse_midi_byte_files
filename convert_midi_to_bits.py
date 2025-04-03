f6e177f35c4760ae0239d3afab61c73a86b89ddd
Ruchir Kafle <ruchirkafle@gmail.com>
2025-04-03 12:41:46 -0400

    data being successfully converted into midi messages. Still need to reimplement try excepts

--- convert_midi_to_bits.py
+++ convert_midi_to_bits.py
@@ -1,21 +1,35 @@
-import sys
+import sys, mido
 
-byte_data = []
+note_data = []
 
-try:
-    parse_file: str = sys.argv[1]
+# try:
+#     parse_file: str = sys.argv[1]
 
-    try:
-        if ".mid" in parse_file:
-            with open(f"input_byte_files/{parse_file}", "rb") as data:
-                byte_data = [*(format(byte, '08b') for byte in data.read())]
-        else:
-            print("Please ensure: \n\tYour file has the file extension of .mid.")
+#     try:
+#         if ".mid" in parse_file:
+#             # Load MIDI file
+            
+#         else:
+#             print("Please ensure: \n\tYour file has the file extension of .mid.")
 
-    except FileNotFoundError:
-        print("Please ensure: \n\tYour MIDI file is in the input_byte_files folder, \n\tYou properly input the MIDI file's name.")
+#     except FileNotFoundError:
+#         print("Please ensure: \n\tYour MIDI file is in the input_byte_files folder, \n\tYou properly input the MIDI file's name.")
 
-except IndexError:
-    print("Please enter the MIDI file as the first argument.")
+# except IndexError:
+#     print("Please enter the MIDI file as the first argument.")
 
-print(byte_data)
+# print(note_data)
+
+midi_file_name = f"input_byte_files/{sys.argv[1]}"
+midi_file = mido.MidiFile(midi_file_name)
+
+# Print MIDI file details
+print(f"Format: {midi_file.type}, Tracks: {len(midi_file.tracks)}")
+
+# Read and print MIDI messages
+for i, track in enumerate(midi_file.tracks):
+    print(f"\nTrack {i}: {track.name}")
+    for msg in track:
+        note_data.append(msg)
+
+print(note_data)
