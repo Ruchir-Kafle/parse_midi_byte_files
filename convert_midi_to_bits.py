0d5a44464953bfba0293aac2e606daa624f0c5bb
Ruchir Kafle <ruchirkafle@gmail.com>
2025-04-03 10:56:59 -0400

    git ignore updates and program properly gets a file from an argument, with errors being covered by try excepts. I think the data is converting to hexadecimal though...

--- convert_midi_to_bits.py
+++ convert_midi_to_bits.py
@@ -3,11 +3,17 @@ import sys
 try:
     parse_file: str = sys.argv[1]
 
-    if type(parse_file) == "<class 'str'>" and ".mid" in parse_file:
-        with open(parse_file, "rb") as midi_file:
-            data = midi_file.read()
-    else:
-        print("Please properly enter the path and ensure it has a file extension of .mid.")
+    try:
+        if ".mid" in parse_file:
+            with open(f"input_byte_files/{parse_file}", "rb") as midi_file:
+                data = midi_file.read()
+                print(data)
+
+        else:
+            print("Please ensure: \n\tYour file has the file extension of .mid.")
+
+    except FileNotFoundError:
+        print("Please ensure: \n\tYour MIDI file is in the input_byte_files folder, \n\tYou properly input the MIDI file's name.")
 
 except IndexError:
     print("Please enter the MIDI file as the first argument.")
