d2be9883c25fc0369dc046eceba860c30a6187de
Ruchir Kafle <ruchirkafle@gmail.com>
2025-04-15 13:01:39 -0400

    json data not being added properly

--- convert_midi_to_bits.py
+++ convert_midi_to_bits.py
@@ -1,4 +1,4 @@
-import sys, mido
+import sys, mido, json
 
 # try:
 #     parse_file: str = sys.argv[1]
@@ -18,8 +18,8 @@ import sys, mido
 
 # print(note_data)
 
-channel_lengths = {}
-channel_notes = {}
+note_lengths = {}
+note_messages = {}
 meta_data = []
 total_time = 0
 
@@ -39,26 +39,56 @@ for i, track in enumerate(midi_file.tracks):
 
         if isinstance(msg, mido.Message):
             if msg.type == "note_on" or msg.type == "note_off":
-                channel_already_in_dict = False
-                for channel in channel_lengths:
-                    if channel == msg.note:
-                        channel_lengths[msg.note] += 1
-                        channel_notes[msg.note].append(msg)
-                        channel_already_in_dict = True
+                note_already_in_dict = False
+                for note in note_lengths:
+                    if note == msg.note:
+                        note_lengths[msg.note] += 1
+                        note_messages[msg.note].append({"time": total_time, "message": msg})
+                        note_already_in_dict = True
                         break
                 
-                if not channel_already_in_dict:
-                    channel_lengths[msg.note] = 1
-                    channel_notes[msg.note] = [msg]
+                if not note_already_in_dict:
+                    note_lengths[msg.note] = 1
+                    note_messages[msg.note] = [{"time": total_time, "message": msg}]
             else:
                 meta_data.append(msg)
 
-populous_channels = {1: -1, 2: -1, 3: -1, 4: -1}
-for channel in channel_lengths:
-    for populous_channel in populous_channels:
-        if populous_channels[populous_channel] == -1:
-            populous_channels[populous_channel] = channel
+populous_notes = {1: -1, 2: -1, 3: -1, 4: -1}
+for note in note_lengths:
+    for populous_note in populous_notes:
+        if populous_notes[populous_note] == -1:
+            populous_notes[populous_note] = note
             break
-        elif channel_lengths[channel] > channel_lengths[populous_channels[populous_channel]]:
-            populous_channels[populous_channel] = channel
+        elif note_lengths[note] > note_lengths[populous_notes[populous_note]]:
+            populous_notes[populous_note] = note
             break
+
+json_data = {"name": "", "artist": "", "seconds": 0, "units": 0, "notes": []}
+for note in populous_notes:
+    messages = note_messages[populous_notes[note]]
+    for i, message in enumerate(messages):
+        if message["message"].type == "note_on":
+            json_data["notes"].append({"note": note - 1, "position": message["time"], "length": messages[i + 1]["time"] - message["time"]})
+
+        # Not in use code, was working off the assumption that channels were being parsed instead of individual note channels.
+        # As such, much of the code makes the assumption that there are multiple note on and offs simultaneously happen.
+        # Might be useful in the future?
+        # unclosed_on_messages = []
+        # unclosed_off_messages = []
+        # if message["message"].type == "note_on":
+        #     unclosed_on_messages.append(message)
+        # elif message["message"].type == "note_off":
+        #     for i, unclosed_message in enumerate(unclosed_on_messages):
+        #         if unclosed_message.note == message["message"].note:
+        #             json_data["notes"].append({"note": note - 1, "position": unclosed_message["time"], "length": message["time"] - unclosed_message["time"]})
+        #             unclosed_on_messages.pop(i)
+        #             break
+        #     else:
+        #         json_data["notes"].append({"note": note - 1, "position": message["time"], "length": 0})
+
+
+
+with open("output_bit_files/output.json", "w") as file:
+    file.write(json.dumps(json_data))
+
+
